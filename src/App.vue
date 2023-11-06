d7ea4a681e46dae503e3f6b6513937dcfcf95f15
Leonhard Tissen <contact@warze.org>
2023-11-06 14:20:47 +0100

    Fixed font

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,11 @@ export default {
 <style>
 @font-face {
 	font-family: Geist;
-	src: url(./assets/fonts/GeistVF.woff2) type(woff2);
+	src: url(./assets/fonts/GeistVF.woff2) format(woff2);
 }
 #app {
 	font-family: Geist, Helvetica, Arial, sans-serif;
+	font-weight: 100;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 }
